5a7e352b6941a8d8232ac06f91f9e975e0d365de
ricardoaguiar <barrios.dev@gmail.com>
2024-08-25 16:24:54 +0200

    test

--- src/components/Header/Cart.vue
+++ src/components/Header/Cart.vue
@@ -37,7 +37,7 @@ const cart = defineProps({
       :class="['modal', cart.isOpen ? '' : 'off']"
       @click="$emit('closeCart')"
     >
-      <CartButton />
+      <!--      <CartButton />-->
     </div>
   </div>
 </template>
