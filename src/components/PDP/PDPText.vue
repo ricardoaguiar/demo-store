e318df6eb4f613b05136e9c10e8517a358446a9a
ricardo <2197501+ricardoaguiar@users.noreply.github.com>
2026-01-18 15:20:55 +0100

    Merge pull request #47
    * fix: lint clean up

--- src/components/PDP/PDPText.vue
+++ src/components/PDP/PDPText.vue
@@ -27,18 +27,15 @@ const selectedInfo = (index: number): number => (text.active = index)
       <h6>{{ info }}</h6>
     </div>
   </div>
-  <div class="container pt-3">
+  <div class="header-container pt-3">
     <div class="columns">
       <div class="column">
         <p>
-          Discover the epitome of modern elegance with our sleek, minimalist
-          coffee table. Crafted from premium oak wood and featuring a smooth,
-          matte black finish, this table blends effortlessly into any
-          contemporary living space. Its clean lines and understated design make
-          it a versatile piece, while the sturdy construction ensures
-          durability. Perfect for showcasing your favorite books, decorative
-          items, or enjoying a cozy cup of coffee, this table is both functional
-          and stylish, adding a touch of sophistication to your home.
+          Discover the epitome of modern elegance with our sleek, minimalist coffee table. Crafted from premium oak wood
+          and featuring a smooth, matte black finish, this table blends effortlessly into any contemporary living space.
+          Its clean lines and understated design make it a versatile piece, while the sturdy construction ensures
+          durability. Perfect for showcasing your favorite books, decorative items, or enjoying a cozy cup of coffee,
+          this table is both functional and stylish, adding a touch of sophistication to your home.
         </p>
       </div>
     </div>
